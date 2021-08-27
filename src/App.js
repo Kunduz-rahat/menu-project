b362952022fdf08cc4e08792f5ed543bf5d6918f
Kunduz Rahat <rahatova.kunduz@gmail.com>
2021-08-27 18:25:46 +0300

    'ingredients'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import Meals from "./Meals";
-import MealsDetails from "./MealsDetails";
+import MealDetails from "./MealDetails";
+import Browse from "./Browse";
+import Ingredient from "./Ingredient";
 function App() {
   return (
     <Router>
-      <Route exact path='/'> <Meals/></Route>
-      <Route  path='/product/:id'> <MealsDetails/></Route>
+      <main className='main'>
+        <Route exact path='/'> <Meals/></Route>
+        <Route  path='/product/:id'> <MealDetails/></Route>
+        <Route  path='/browse/:name'> <Browse /></Route>
+        <Route  path='/ingredient/:name'> <Ingredient /></Route>
+      </main>
+
     </Router>
   );
 }
--- src/Meals.js
+++ src/Meals.js
@@ -1,30 +1,52 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 
-const Meals =() =>{
-const [meals, setMeals] = useState([])
-  useEffect(() =>{
+
+const Meals = () => {
+  const [meals, setMeals] = useState([])
+  const [seacrh, setSearch] = useState('')
+  const history = useHistory()
+
+  const handleSeacrh = (e) => {
+    setSearch(e.target.value)
+  }
+  const handleClick = () => {
+    if (seacrh.trim()){
+      history.push(`/browse/${seacrh}`)
+    }
+  }
+  useEffect(() => {
     axios('https://www.themealdb.com/api/json/v2/1/randomselection.php')
-      .then(({data})=> setMeals(data.meals))
+      .then(({data}) => setMeals(data.meals))
   }, [])
   return (
     <div>
-      <h1>Menu</h1>
+      <h1>Latest Meals</h1>
+      <form className='form'>
+        <input type="text" onChange={handleSeacrh} placeholder='Search...' className='form_search'/>
+        <button type="button" onClick={handleClick} className='form_search'>Search</button>
+      </form>
+
+
       <div className='row'>
 
         {
           meals.map(item => (
               <div key={item.idMeal}>
-                <img src={item.strMealThumb} alt="" className='meals_img'/>
-                <Link  to={`/product/${item.idMeal}`} clasName='link'><div>{item.strMeal}</div></Link>
+
+                <Link to={`/product/${item.idMeal}`} clasName='link'>
+                  <img src={item.strMealThumb} alt="" className='meals_img'/>
+                  <div>{item.strMeal}</div>
+                </Link>
               </div>
             )
           )
         }
 
+
       </div>
     </div>
   )
 }
-  export default Meals
+export default Meals
