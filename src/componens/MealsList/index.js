b8cbc86c07970d8ac3b3981e5c8241470c30668f
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-04-29 21:36:37 +0600

    add categories

--- src/componens/MealsList/index.js
+++ src/componens/MealsList/index.js
@@ -2,25 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 const MealsList = ({ meals }) => {
   return (
-    <div className='h-screen flex items-center justify-center bg-white'>
-      <div className="grid grid-cols-12 px-18 gap-5">
+    <div >
+      
         {
           meals.map(item => (
             <div key={item.idMeal}>
-
-              <Link to={`/product/${item.idMeal}`} className='col-span-3 bg-rose-700 rounded-xl h-52 md:h-80'>
-                <img src={item.strMealThumb} alt="" className='rounded-t-xl max-h-44' />
-                <p className='text-xl md:text-3xl text-gray-50 pt-5 pl-3'>{item.strMeal} </p>
+              <Link to={`/product/${item.idMeal}`} >
+               
+                <img alt="example" src={item.strMealThumb} />
+                
+                  <h2>{item.strMeal}</h2>
+               
               </Link>
             </div>
           )
           )
         }
-      </div>
-
-
-
-
     </div>
   )
 }
--- src/views/Categories/index.js
+++ src/views/Categories/index.js
@@ -1,22 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import MealsList from "../../componens/MealsList";
+import {Link} from "react-router-dom";
+import MealsList from '../../componens/MealsList';
+
+
 
 
 
 
 const Categories = () => {
   const [meals, setMeals] = useState([])
-  
+
   useEffect(() => {
     axios(`https://www.themealdb.com/api/json/v1/1/categories.php`)
-      .then(({data}) =>setMeals (data.categories))
+
+      .then(({ data }) => (setMeals(data.categories)))
+
   }, [])
+
   return (
-  <div>
-   <MealsList meals={meals}/>
-  олжлжлжлjlljl
-  </div>
+    <div>
+      {
+        meals.map(item => (
+          <div key={item.idCategory} >
+
+            <Link to={`/categories/${item.idCategory}`} className='link'>
+              <img src={item.strCategoryThumb} alt="" className='meals_img' />
+              <p>{item.strCategory}</p>
+            </Link>
+
+          </div>
+        )
+        )
+      }
+
+
+    </div>
   )
 };
 
